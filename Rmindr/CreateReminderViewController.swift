7a2c860f953060606eb0c37b42616903bc5e32d8
rondale.williams <rondale.williams@disney.com>
2017-08-30 16:34:20 -0700

    Added navigation controller in Main.storyboard as Initial ViewController, and added implementation of title label and date label data.

--- Rmindr/CreateReminderViewController.swift
+++ Rmindr/CreateReminderViewController.swift
@@ -10,4 +10,38 @@ import UIKit
 
 class CreateReminderViewController: UIViewController {
   
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var titleTextField: UITextField!
+  var reminder: Reminder?
+  
+  @IBAction func openDatePicker(_ sender: Any) {
+    let datePicker = UIDatePicker(frame: CGRect(x: 0.0, y: 0.0, width: self.view.bounds.size.width, height: 40.0))
+    datePicker.datePickerMode = .dateAndTime
+    datePicker.isHidden = false
+    datePicker.date = Date()
+    datePicker.addTarget(self, action: #selector(getReminderDate), for: .valueChanged)
+    view.addSubview(datePicker)
+  }
+  
+  func getReminderDate(date: Date) {
+    reminder?.date = date as NSDate
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .medium
+    formatter.amSymbol = "AM"
+    formatter.pmSymbol = "PM"
+    
+    if let date = reminder?.date {
+      let reminderDate = date as Date
+      let dateString = formatter.string(from: reminderDate)
+      self.dateLabel.text = dateString
+    }
+    
+  }
+}
+
+extension CreateReminderViewController: UITextFieldDelegate {
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    reminder?.title = textField.text
+  }
 }
